5f04855fbf658576816d0c81e89bd9bdd1053ef2
Mathias Salva <mattsalva@outlook.com>
2021-08-24 23:52:08 -0300

    q and f objects

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Q, F
 from django.http import HttpResponse
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range=(20, 30))
+    query_set = Product.objects.filter(inventory=F('collection__id'))
+    # | Q(inventory__lt=10) | Q(unit_price__lt=20)
     #product = Product.objects.filter(pk=1).first()
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
